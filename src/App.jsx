57d22d43960cb4e0a778a591d7d2c21daea1552e
theninexl <thenine@thenine.es>
2023-12-08 14:07:02 +0100

    Step 9. Adding a loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Col } from 'antd';
+import { Col, Spin } from 'antd';
 import { Searcher } from './components/Searcher';
 import { PokemonList } from './components/PokemonList';
 import logo from './statics/logo.svg';
 import { getPokemon } from './api';
-import { getPokemonsWithDetails } from './actions';
+import { getPokemonsWithDetails, setLoading } from './actions';
 import './App.css';
 
 
@@ -13,12 +13,15 @@ import './App.css';
 function App() {  
 
   const pokemons = useSelector(state => state.pokemons);
+  const loading = useSelector(state => state.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      dispatch(setLoading(true));
       const pokemonsRes = await getPokemon();
       dispatch(getPokemonsWithDetails(pokemonsRes));
+      dispatch(setLoading(false));
     };
 
     fetchPokemons();
@@ -35,7 +38,13 @@ function App() {
         <Col span={8} offset={8}>
           <Searcher />
         </Col>
-        <PokemonList pokemons={pokemons} />
+        {loading ? 
+          <Col offset={12}>
+            <Spin spinning size='large' />
+          </Col>
+          : 
+          <PokemonList pokemons={pokemons} />
+          }
       </div>
     </>
   )
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,7 +1,8 @@
-import { SET_POKEMONS } from "../actions/types";
+import { SET_LOADING, SET_POKEMONS } from "../actions/types";
 
 const initialState = {
   pokemons: [],
+  loading: false,
 };
 
 export const pokemonsReducer = (state = initialState, action) => {
@@ -10,6 +11,11 @@ export const pokemonsReducer = (state = initialState, action) => {
       return {
         ...state,
         pokemons: action.payload
+      }
+    case SET_LOADING: 
+      return{
+        ...state,
+        loading: action.payload
       }
     default: 
       return state;
