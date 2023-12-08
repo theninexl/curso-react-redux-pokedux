ac72b515cb7e4d5577d66002b913d09320e0e872
theninexl <thenine@thenine.es>
2023-12-08 11:35:27 +0100

    Step 6. Adding middlewares

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { pokemonsReducer } from './reducers/pokemons.js';
 import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import { compose, applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { logger, featuring, numberingPokes } from './middlewares/index.js';
 import './index.css'
 import'antd/dist/reset.css';
 
-const store = createStore(
-                pokemonsReducer,
-                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                );
+const composeEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring, numberingPokes)
+)
+
+const store = createStore(pokemonsReducer, composeEnhancers);
 
 ReactDOM.createRoot(document.getElementById('root')).render(  
   <React.StrictMode>
